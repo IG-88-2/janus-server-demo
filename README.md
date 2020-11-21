c9a4c0ccc28400e65cdb22a69d7963713b043750
IG-88-2 <anatoly.strashkevich@gmail.com>
2020-11-21 10:25:04 +0200

    dev

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # janus-server-demo
+This repo contains demo which is based upon [janus-gateway-node](https://github.com/IG-88-2/janus-gateway-node) package.
+![alt text](https://github.com/IG-88-2/janus-server-demo/blob/master/example.jpg?raw=true)
+
+## DEMO
+
+[link](https://kreiadesign.com/)
+
+## Contributing
+Please consider to help by providing feedback on how this project can be 
+improved or what is missing to make it useful for community. Thank you!
+## Authors
+
+* **Anatoly Strashkevich**
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Build
+git clone https://github.com/IG-88-2/janus-server-demo
+git submodule update --init --recursive
+install & build submodules
+yarn install
+npm run build
+npm run start (use your public ip in command)
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { Janus } from 'janus-gateway-node';
+const { argv } = require('yargs');
 import express = require("express");
 import cors = require("cors");
 import bodyParser = require("body-parser");
@@ -6,116 +7,15 @@ const pause = (n:number) => new Promise((resolve) => setTimeout(() => resolve(),
 const path = require(`path`);
 const fs = require('fs');
 const util = require('util');
-const logFile = fs.createWriteStream(__dirname + '/test.log', { flags : 'w' });
-//const roomsFile = fs.createWriteStream(__dirname + '/rooms.log', { flags : 'a' });
-const { argv } = require('yargs');
-const transformError = (error: Object | string) => !error ?  `Unknown error` : typeof error === "string" ? error : util.inspect(error, {showHidden: false, depth: null});
+const logFile = fs.createWriteStream(__dirname + '/test.log', { flags: 'w' });
+const transformError = (error: Object | string) => !error ?  `Unknown error` : typeof error === "string" ? error : util.inspect(error, { showHidden: false, depth: null });
 const https = require('https');
 const router = express.Router();
 const app = express();
+
 const p = path.join(__dirname, "development");
+
 let janus = null;
-
-
-
-const setAllowed = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-
-    res.header("Access-Control-Allow-Methods", "HEAD, PUT, POST, GET, OPTIONS, DELETE");
-
-    res.header("Access-Control-Allow-Headers", "origin, content-type, Authorization, x-access-token");
-
-    if (req.method === "OPTIONS") {
-        return res
-            .status(405)
-            .send()
-            .end();
-    } else {
-        next();
-    }
-};
-
-app.use(express.static(p));
-
-router.get("/", (req, res) => {
-    res.json({
-        version: "0.0.3"
-    });
-});
-
-router.post("/room", async (req, res, next) => {
-
-	try {
-
-		const { description, videocodec, vp9_profile } = req.body;
-
-		const load = {
-			description: `room ${Math.round(Math.random() * 1000)}`,
-			bitrate: 512000,
-			bitrate_cap: false,
-			fir_freq: undefined,
-			videocodec: "vp9",
-			permanent: true,
-			vp9_profile: "1"
-		};
-
-		if (description) {
-			load.description = String(description);
-		}
-
-		if (videocodec) {
-			load.videocodec = String(videocodec);
-		}
-
-		if (vp9_profile) {
-			load.vp9_profile = String(vp9_profile);
-		}
-
-		logger.info(`creating room... ${description}`);
-
-		const result = await janus.createRoom({ load });
-
-		if (result && result.load && result.load.context) {
-			const id = result.load.context.room_id;
-			//roomsFile.write(`${id}\n`);
-		}
-
-		const response = {
-			success: true,
-			code: 200,
-			data: result
-		};
-
-		return res.json(response);
-
-	} catch(error) {
-
-		logger.info(`creating room error...`);
-
-		logger.error(error);
-
-		const response = {
-			success: false,
-			code: 500,
-			data: error
-		};
-
-		return res.json(response);
-
-	}
-
-});
-
-app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(bodyParser.json());
-
-app.use(setAllowed);
-
-app.use("/v1", router);
-
 
 let enable = true;
 
@@ -178,75 +78,122 @@ const logger = {
 	}
 };
 
+const setAllowed = (req, res, next) => {
 
+    res.header("Access-Control-Allow-Origin", "*");
 
-const retrieveContext = () => {
+    res.header("Access-Control-Allow-Methods", "HEAD, PUT, POST, GET, OPTIONS, DELETE");
+
+    res.header("Access-Control-Allow-Headers", "origin, content-type, Authorization, x-access-token");
+
+    if (req.method === "OPTIONS") {
+        return res
+            .status(405)
+            .send()
+            .end();
+    } else {
+        next();
+	}
+	
+};
+
+/**
+ * when request to server is made - use janus.createRoom method from janus instance
+ * to create new room and send its info back in response
+ */
+const postNewRoom = async (req, res) => {
 
 	try {
 
-		const contextPath = path.resolve('context.json');
+		const { description, videocodec, vp9_profile } = req.body;
 
-		const file = fs.readFileSync(contextPath, 'utf-8');
+		const load = {
+			description: `room ${Math.round(Math.random() * 1000)}`,
+			bitrate: 512000,
+			bitrate_cap: false,
+			fir_freq: undefined,
+			videocodec: "vp9",
+			permanent: true,
+			vp9_profile: "1"
+		};
 
-		const context = JSON.parse(file);
+		if (description) {
+			load.description = String(description);
+		}
 
-		return context;
+		if (videocodec) {
+			load.videocodec = String(videocodec);
+		}
+
+		if (vp9_profile) {
+			load.vp9_profile = String(vp9_profile);
+		}
+
+		logger.info(`creating room... ${description}`);
+
+		const result = await janus.createRoom({ load });
+
+		//if (result && result.load && result.load.context) {
+		//	const id = result.load.context.room_id;
+		//}
+
+		const response = {
+			success: true,
+			code: 200,
+			data: result
+		};
+
+		return res.json(response);
 
 	} catch(error) {
 
+		logger.info(`creating room error...`);
+
 		logger.error(error);
 
-		return {};
+		const response = {
+			success: false,
+			code: 500,
+			data: error
+		};
+
+		return res.json(response);
 
 	}
 
 };
 
+app.use(express.static(p));
 
+router.get("/", (req, res) => {
+    res.json({
+        version: "0.0.41"
+    });
+});
 
-const updateContext = async (rooms) => {
+router.post("/room", postNewRoom);
 
-	try {
-		
-		const contextPath = path.resolve('context.json');
+app.use(cors());
 
-		const file = JSON.stringify(rooms);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-		const fsp = fs.promises;
+app.use(bodyParser.json());
 
-		await fsp.writeFile(contextPath, file, 'utf8');
-		
-	} catch(error) {
+app.use(setAllowed);
 
-		logger.error(error);
-		
-	}
+app.use("/v1", router);
 
-	return rooms;
-
-}
-
-
-
-const main = async () => {
-	
-	const nRooms = 5;
-
-	const coy = argv.coy;
-
-	console.log('start', argv, coy);
+const launchServer = () => {
 
 	const paths = {
-		key:coy ? "/home/vmadmin/ssl/blipiq.key" : "/etc/letsencrypt/live/kreiadesign.com/privkey.pem", //STAR_blipiq_com.ca-bundle
-		cert:coy ? "/home/vmadmin/ssl/STAR_blipiq_com.crt" : "/etc/letsencrypt/live/kreiadesign.com/cert.pem"
+		key:"/etc/letsencrypt/live/kreiadesign.com/privkey.pem",
+		cert:"/etc/letsencrypt/live/kreiadesign.com/cert.pem"
 	};
 
 	const keys = {
 		key:fs.readFileSync(paths.key),
 		cert:fs.readFileSync(paths.cert)
 	};
-	
-	console.log(keys);
 
 	const serverOptions = { 
 		key: keys.key,
@@ -255,74 +202,27 @@ const main = async () => {
 	
 	const server = https.createServer(serverOptions, app).listen(443); 
 	
-	await pause(3000);
-	
-	let instances = null;
+	return server;
 
+};
+
+const grabRunningInstances = () => {
+	
+	let instances = undefined;
+	//if local file exist with instances descriptions - use it to retrieve instances configuration
+	//otherwise pass undefined to constructor and it will generate instances by itself 
 	try {
 		const instancesPath = path.resolve('instances.json');
 		const result = fs.readFileSync(instancesPath, 'utf-8');
 		instances = JSON.parse(result);
 	} catch(e) {}
-	
-	const generateInstances = instances ? () => instances : undefined;
-	
-	janus = new Janus({
-		generateInstances,
-		logger,
-		onError:(error) => {
-			
-			logger.error(error);
 
-		},
-		publicIp:coy ? '40.87.103.74' : '18.158.159.40',
-		webSocketOptions:{
-			server
-		}
-	});
+	return instances;
 
-	await janus.initialize();
+};
 
-	let list : string[] = [];
-	
-	try {
+const createDummyRoomsForFun = async (nRooms) => {
 
-		const rooms = fs.readFileSync(__dirname + '/rooms.log');
-
-		list = rooms.toString().split('\n').filter((s) => s.length > 0);
-
-	} catch(error) {
-		
-		console.error(error);
-	
-	}
-	
-	/*
-	console.log('rooms loaded', list);
-	
-	for(let i = 0; i < list.length; i++) {
-		const id = list[i].trim();
-		console.log(`recreating room ${id}...`);
-		try {
-			const result = await janus.createRoom({
-				load: {
-					id,
-					description: i%2 ? `Restore cool vp8 room (${i})` : `Restore cool vp9 room (${i})`,
-					bitrate: 512000,
-					bitrate_cap: false,
-					fir_freq: undefined,
-					videocodec: i===0 ? "vp8" : "vp9",
-					vp9_profile: i===0 ? undefined : "1",
-					permanent: true
-				}
-			});
-			console.log(`recreating room ${id} result...`, result.load);
-		} catch(error) {
-			console.error(error);
-		}
-	}
-	*/
-	
 	for(let i = 0; i < nRooms; i++) {
 		logger.info(`creating room ${i + 1}...`);
 		const result = await janus.createRoom({
@@ -338,12 +238,45 @@ const main = async () => {
 		});
 		logger.info(`creating room ${i + 1} result...`);
 		console.log(result.load);
-		const id = result.load.context.room_id;
-		//roomsFile.write(`${id}\n`);
+		//const id = result.load.context.room_id;
 		logger.info(`room ${i + 1} created...`);
 		logger.json(result);
 	}
+
+};
+
+/**
+ * janus gateway node usage example
+ */
+const main = async () => {
 	
-}
+	const nRooms = 5;
+
+	const publicIp = argv.ip;
+
+	const server = launchServer();
+	
+	await pause(3000);
+	
+	//we need to get dockerized janus instances up and running
+	let instances = grabRunningInstances();
+	
+	const generateInstances = instances ? () => instances : undefined;
+	
+	janus = new Janus({
+		generateInstances,
+		logger,
+		onError:(error) => logger.error(error),
+		publicIp,
+		webSocketOptions:{
+			server
+		}
+	});
+
+	await janus.initialize();
+	
+	await createDummyRoomsForFun(nRooms);
+	
+};
 
 main();
