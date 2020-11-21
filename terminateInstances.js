079baad4aa941f307f1989b8832ecb8e00db7747
IG-88-2 <anatoly.strashkevich@gmail.com>
2020-11-21 12:04:44 +0200

    dev

--- terminateInstances.js
+++ terminateInstances.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const terminateInstances = async () => {
 		
-    const command = process.platform==='linux' ? `docker rm $(docker ps -a -q)` : `FOR /F %A IN ('docker ps -q') DO docker rm -f %~A`;
+    const command = process.platform==='linux' ? `docker rm -f $(docker ps -a -q)` : `FOR /F %A IN ('docker ps -q') DO docker rm -f %~A`;
     
     try {
 
