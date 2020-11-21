b4acbbc5500fd4b383565ee1ec6fdadfb2cce4a0
IG-88-2 <anatoly.strashkevich@gmail.com>
2020-11-21 13:06:02 +0200

    dev

--- launchInstances.js
+++ launchInstances.js
@@ -82,7 +82,8 @@ const launchInstances = async () => {
         //--network=host
         //-p 127.0.0.1:20000-40000:20000-40000
         //command += `-p 127.0.0.1:${udpStart}-${udpEnd}:${udpStart}-${udpEnd}/udp `;
-        command += `-p ${docker_ip}:${udpStart}-${udpEnd}:${udpStart}-${udpEnd}/udp `;
+        //command += `-p ${docker_ip}:${udpStart}-${udpEnd}:${udpStart}-${udpEnd}/udp `;
+        command += `-p ${udpStart}-${udpEnd}:${udpStart}-${udpEnd}/udp `;
         command += `-p ${ws_port}:${ws_port} `;
         command += `-p ${admin_ws_port}:${admin_ws_port} `;
         command += `${args.map(([name,value]) => `-e ${name}="${value}"`).join(' ')} `;
