4c5d274839d3b91dc74daa73b70b40b1691b801f
IG-88-2 <anatoly.strashkevich@gmail.com>
2021-10-15 15:40:56 +0300

    dev

--- launchInstances.ts
+++ launchInstances.ts
@@ -58,7 +58,7 @@ const launchInstances = async (config) => {
             ws_port : start_ws_port + i,
             admin_ws_port : start_admin_ws_port + i,
             stun_server : "stun.voip.eutelia.it",
-            nat_1_1_mapping : `127.0.0.${1 + i}`, //"127.0.0.1", //"3.121.126.200",
+            nat_1_1_mapping : "3.121.126.200", //`127.0.0.${1 + i}`, //"127.0.0.1",
             stun_port : 3478,
             debug_level : 5 //6
         };
@@ -98,8 +98,8 @@ const launchInstances = async (config) => {
         //--network=host
         //command += `-p 127.0.0.1:${udpStart}-${udpEnd}:${udpStart}-${udpEnd}/udp `;
         
-        command += `-p ${udpStart}-${udpEnd}:${udpStart}-${udpEnd}/udp `;
-        //command += `-p ${docker_ip}:${udpStart}-${udpEnd}:${udpStart}-${udpEnd}/udp `;
+        //command += `-p ${udpStart}-${udpEnd}:${udpStart}-${udpEnd}/udp `;
+        command += `-p ${docker_ip}:${udpStart}-${udpEnd}:${udpStart}-${udpEnd}/udp `;
         command += `-p ${ws_port}:${ws_port} `;
         command += `-p ${admin_ws_port}:${admin_ws_port} `;
         command += `${args.map(([name,value]) => `-e ${name}="${value}"`).join(' ')} `;
